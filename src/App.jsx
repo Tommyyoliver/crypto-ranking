49e5d739750cf50715273d9e84d4d0fcefeb19d8
Tommy Oliver <tommyolivr@gmail.com>
2023-01-27 17:29:20 -0300

    Barra de busqueda agregado

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import logoCrypto from "../public/logo-crypto.png";
 function App() {
 
   const [cryptos, setCryptos] = useState([]);
+  const [search, setSearch] = useState("");
 
   const apiFetch = async() => { 
     const api = await fetch("https://api.coingecko.com/api/v3/coins");
@@ -16,6 +17,13 @@ function App() {
     apiFetch();
   }, [])
 
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  }
+
+  let results = !search ? cryptos : cryptos.filter(crypto => crypto.name.toLowerCase().includes(search.toLocaleLowerCase()));
+
+
   return (
     <div className="App">
       <h2>Crypto Ranking</h2>
@@ -25,14 +33,22 @@ function App() {
         </div>
       ) : (
       <div className="table">
+        <div className="search">
+          <input type="text" placeholder="Buscar..." onChange={handleSearch}/>
+        </div>
         <div className="head">
           <div className="rank"><h3>RANGO</h3></div>
           <div className="name"><h3>NOMBRE</h3></div>
           <div className="symbol"><h3>SIMBOLO</h3></div>
           <div className="priceDay"><h3>PRECIO 24HS</h3></div>
         </div>
+        {results.length === 0 ? (
+          <div className="not-found">
+            <h3>No encontrado</h3>
+          </div>
+        ) : (
         <div className="body">
-          {cryptos.map(crypto => (
+          {results.map(crypto => (
             <div key={crypto.id} className="money">
               <div className="money-rank">{crypto.market_data.market_cap_rank}</div>
               <div className="money-name"><img src={crypto.image.small} alt="icon" />{crypto.name}</div>
@@ -48,6 +64,7 @@ function App() {
             </div>
           ))}
         </div>
+        )}
       </div>
       )}
       <footer>
